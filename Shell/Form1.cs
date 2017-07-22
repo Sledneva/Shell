ac949d858b96ebb18b58aa970187eeeb631c116b
Sledneva <slednevanatasha@gmail.com>
2017-07-22 15:16:56 +0300

    Add files via upload

---
+++ Shell/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Shell sh = new Shell();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string str = textBox1.Text;
+            int[] num = sh.Sort(Shell.Parse(str), ref count);
+            label3.Text = count.ToString();
+            for (int i = 0; i < num.Length; i++)
+            {
+                textBox2.Text += num[i] + " ";
+            }
+        }
+    }
+}
---
+++ Shell/Shell.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell
+{
+    class Shell
+    {
+        public static int[] Parse(string input)
+        {
+            string[] arr = input.Split(' ', ',', '.');
+            int[] num = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+                num[i] = int.Parse(arr[i]);
+            return num;
+        }
+        public int[] Sort(int[] num, ref int count )
+        {
+            int j;
+            int step = num.Length / 2;
+            while (step > 0)
+            {
+                for (int i = 0; i < (num.Length - step); i++)
+                {
+                    j = i;
+                    while ((j >= 0) && (num[j] > num[j + step]))
+                    {
+                        count++;
+                        int tmp = num[j];
+                        num[j] = num[j + step];
+                        num[j + step] = tmp;
+                        j -= step;
+                    }
+                    count++;
+                }
+                step = step / 2;
+            }
+            
+            return num;
+        }
+
+            public int[] Sort2(int[] num)
+        {
+            int j;
+            int step = num.Length / 2;
+            while (step > 0)
+            {
+                for (int i = 0; i < (num.Length - step); i++)
+                {
+                    j = i;
+                    while ((j >= 0) && (num[j] > num[j + step]))
+                    {
+                        int tmp = num[j];
+                        num[j] = num[j + step];
+                        num[j + step] = tmp;
+                        j -= step;
+                    }
+                }
+                step = step / 2;
+            }
+            return num;
+        }
+    }
+}
